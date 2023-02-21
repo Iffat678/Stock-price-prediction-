d3de410751ebc33b955b128b0d7bbd9c4ddf3800
Iffat khan <110293997+Iffat678@users.noreply.github.com>
2023-02-21 11:12:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load the stock data
-df = pd.read_csv('stock_data.csv')
+st.title('Stock Price Prediction')
+st.markdown("Make sure that the columns in the csv are labelled as 'DATE', 'Open',  'High',  'Low', 'Close', for no errors")
+st.write('Upload a CSV file to generate Stock price Prediction:')
 
-# Plot the stock price
-st.line_chart(df.set_index('date')['close'])
+uploaded_file = st.file_uploader('Choose a CSV file', type='csv')
+if uploaded_file is None:
+    st.write("No file uploaded. Please upload a file containing data.")
+else:
+    df = pd.read_csv(uploaded_file)
+    if df.empty:
+        st.write("The uploaded file is empty. Please upload a file containing data.")
+    else:
+        st.line_chart(df.set_index('Date')['Close'])
 
-# Train the model
-from sklearn.linear_model import LinearRegression
-model = LinearRegression()
-model.fit(df[['open', 'high', 'low']], df['close'])
+        # Train the model
+        from sklearn.linear_model import LinearRegression
+        model = LinearRegression()
+        model.fit(df[['Open', 'High', 'Low']], df['Close'])
 
-# Predict the stock price
-def predict_price(model, data):
-    return model.predict(data)
+        # Predict the stock price
+        def predict_price(model, data):
+            return model.predict(data)
 
-# Get the input data
-open_price = st.number_input('Open price')
-high_price = st.number_input('High price')
-low_price = st.number_input('Low price')
-
-# Make the prediction
-if st.button('Predict'):
-    prediction = predict_price(model, np.array([[open_price, high_price, low_price]]))
-    st.write('The predicted stock price is: ', prediction[0])
+        # Get the input data
+        open_price = st.number_input('Open price')
+        high_price = st.number_input('High price')
+        low_price = st.number_input('Low price')
+        plt.scatter(df['Open'], df['Close'])
+        plt.show()
+        
+        # Make the prediction
+        if st.button('Predict'):
+            prediction = predict_price(model, np.array([[open_price, high_price, low_price]]))
+            st.write('The predicted stock price is: ', prediction[0])
